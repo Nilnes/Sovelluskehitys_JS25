a3975aae865a021c5c0232a2a96c53de9d1bea2a
Nilnes <jenny.saukko@hotmail.com>
2025-11-07 13:00:50 +0200

    Näkymän muokkaus ja siirtäminen tabeihin

--- Sovelluskehitys_JS25/MainWindow.xaml.cs
+++ Sovelluskehitys_JS25/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace Sovelluskehitys_JS25
         public MainWindow()
         {
             InitializeComponent();
-            Update_DataGrid(this, null);
+            Update_DataGrid("SELECT * FROM products", "product", productlist);
             Update_ComboBox(this, null);
         }
 
@@ -35,7 +35,7 @@ namespace Sovelluskehitys_JS25
 
             connection.Close();
 
-            Update_DataGrid(sender, e);
+            Update_DataGrid("SELECT * FROM products","product", productlist);
             Update_ComboBox(sender, e);
 
             Tex1.Clear();
@@ -43,38 +43,48 @@ namespace Sovelluskehitys_JS25
             Tex3.Clear();
         }
 
-        private void Update_DataGrid(object sender, RoutedEventArgs e)
+        private void Update_DataGrid(string query, string table_name, DataGrid grid)
+        {
+            SqlConnection connection = new SqlConnection(path);
+            connection.Open();
+
+            SqlCommand command = connection.CreateCommand();
+            command.CommandText = query;
+
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable("products");
+            adapter.Fill(dataTable);
+
+            grid.ItemsSource = dataTable.DefaultView;
+
+            connection.Close();
+        }
+
+        // cb_products = ComboBox
+        private void Update_ComboBox(object sender, RoutedEventArgs e)
         {
             SqlConnection connection = new SqlConnection(path);
             connection.Open();
 
             string query = "SELECT * FROM products";
             SqlCommand command = new SqlCommand(query, connection);
-
-            SqlDataAdapter adapter = new SqlDataAdapter(command);
-            DataTable dataTable = new DataTable("products");
-            adapter.Fill(dataTable);
-
-            productlist.ItemsSource = dataTable.DefaultView;
-
-            connection.Close();
-        }
-
-        // cb = ComboBox
-        private void Update_ComboBox(object sender, RoutedEventArgs e)
-        {
-           SqlConnection connection = new SqlConnection(path);
-            connection.Open();
-
-            string query = "SELECT * FROM products";
-            SqlCommand command = new SqlCommand(query, connection);
             SqlDataReader reader = command.ExecuteReader();
 
-            cb_products.Items.Clear();
+            // Datataulu comboboxin sisältöä varten
+            DataTable dataTable = new DataTable();
+            dataTable.Columns.Add("id", typeof(string));
+            dataTable.Columns.Add("name", typeof(string));
+
+            /* tehdään sidos että comboxissa näytetään datataulu */
+            cb_products.ItemsSource = dataTable.DefaultView;
+            cb_products.DisplayMemberPath = "name";
+            cb_products.SelectedValuePath = "id";
 
             while (reader.Read())
             {
-                cb_products.Items.Add(reader.GetString(1));
+                int id = reader.GetInt32(0);
+                string name = reader.GetString(1);
+                dataTable.Rows.Add(id, name);
             }
 
             reader.Close();
@@ -83,7 +93,33 @@ namespace Sovelluskehitys_JS25
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            Update_DataGrid(sender, e);
+            Update_DataGrid("SELECT * FROM products", "product", productlist);
+
+        }
+
+        private void delete_product_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(path);
+            connection.Open();
+
+            if (cb_products.SelectedValue == null)
+            {
+                MessageBox.Show("Valitse poistettava tuote.");
+                return;
+            }
+
+            string id = cb_products.SelectedValue.ToString();
+            MessageBox.Show("Poistettu tuote ID: " + id);
+
+            string query = "DELETE FROM products WHERE id = " + id + ";";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.ExecuteNonQuery();
+
+
+            connection.Close();
+
+            Update_DataGrid("SELECT * FROM products", "product", productlist);
+            Update_ComboBox(sender, e);
         }
     }
 }
